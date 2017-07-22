3a02711d7ea473bbcf22194be1bce1947a8b8e5a
embulldogs99 <embulldogs99@yahoo.com>
2017-07-22 16:27:09 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+  "net/http"
+  	"html/template"
+    "log"
+)
+
+
+func main() {
+
+  s := &http.Server{
+
+    Addr:    ":8080",
+    Handler: nil,
+  }
+
+  http.Handle("/favicon/", http.StripPrefix("/favicon/", http.FileServer(http.Dir("./favicon"))))
+  http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+  http.HandleFunc("/", serve)
+  http.HandleFunc("/about", serveabout)
+  http.HandleFunc("/contact", servecontact)
+  log.Fatal(s.ListenAndServe())
+}
+
+func serve(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("main.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl.Execute(w, nil)
+}
+
+func serveabout(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("about.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl.Execute(w, nil)
+}
+
+func servecontact(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl.Execute(w, nil)
+}
