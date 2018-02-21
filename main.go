52fe09cab48244c35a7dd0aedc095270559396bd
embulldogs99 <embulldogs99@yahoo.com>
2018-02-20 20:49:37 -0800

    automated posting

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import(
 func main() {
 
   s := &http.Server{
-
     Addr:    ":80",
     Handler: nil,
   }
@@ -18,9 +17,11 @@ func main() {
   http.Handle("/favicon/", http.StripPrefix("/favicon/", http.FileServer(http.Dir("./favicon"))))
   http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+  http.Handle("/research/", http.StripPrefix("/research/", http.FileServer(http.Dir("./research"))))
   http.HandleFunc("/", serve)
   http.HandleFunc("/about", serveabout)
   http.HandleFunc("/contact", servecontact)
+  http.HandleFunc("/contact", researchlinks)
   log.Fatal(s.ListenAndServe())
 }
 
@@ -39,5 +40,11 @@ func serveabout(w http.ResponseWriter, r *http.Request){
 func servecontact(w http.ResponseWriter, r *http.Request){
   var tpl *template.Template
   tpl = template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl.Execute(w, nil)
+}
+
+func researchlinks(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("researchlinks.gohtml","css/main.css","css/mcleod-reset.css" ))
   tpl.Execute(w, nil)
 }
