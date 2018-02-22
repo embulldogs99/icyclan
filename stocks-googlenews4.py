309f82f45620b5d56783c06d6d1602d81a0557f2
embulldogs99 <embulldogs99@yahoo.com>
2018-02-21 22:58:31 -0800

    automated posting

--- stocks-googlenews4.py
+++ stocks-googlenews4.py
@@ -815,7 +815,7 @@ def contentfilter():
 							print("about to insert a value")
 							time.sleep(5)
 
-							conn = psychopq2.connect("host='localhost' dbname='fmi' user='postgres' password='rk'")
+							conn = psychopg2.connect("host='localhost' dbname='fmi' user='postgres' password='rk'")
 							#create a cursor
 
 							print("successfully connected")
