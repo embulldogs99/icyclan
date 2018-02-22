a8fcdc9c880df91b3c8cb2f82d13bfd1c313cf55
embulldogs99 <embulldogs99@yahoo.com>
2018-02-21 19:06:32 -0800

    automated posting

--- main.go
+++ main.go
@@ -18,11 +18,18 @@ func main() {
   http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
   http.Handle("/research/", http.StripPrefix("/research/", http.FileServer(http.Dir("./research"))))
+  
   http.HandleFunc("/", serve)
   http.HandleFunc("/about", serveabout)
   http.HandleFunc("/contact", servecontact)
-  http.HandleFunc("/research", researchlinks)
+  http.HandleFunc("/research", s(w,r,"researchlinks.gohtml"))
   log.Fatal(s.ListenAndServe())
+}
+
+fun s(w http.ResponseWriter, r *http.Request, html string){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles(html,"css/main.css","css/mcleod-reset.css"))
+  tpl.Execute(w, nil)
 }
 
 func serve(w http.ResponseWriter, r *http.Request){
@@ -40,11 +47,5 @@ func serveabout(w http.ResponseWriter, r *http.Request){
 func servecontact(w http.ResponseWriter, r *http.Request){
   var tpl *template.Template
   tpl = template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
-  tpl.Execute(w, nil)
-}
-
-func researchlinks(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("researchlinks.gohtml","css/main.css","css/mcleod-reset.css" ))
   tpl.Execute(w, nil)
 }
