df882af0b97af6afa9473e6de5849e282aa26041
embulldogs99 <embulldogs99@yahoo.com>
2018-02-21 19:52:17 -0800

    automated posting

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
-
 import(
   "net/http"
   	"html/template"
     "log"
 )
-
 
 func main() {
 
@@ -22,30 +20,23 @@ func main() {
   http.HandleFunc("/", serve)
   http.HandleFunc("/about", serveabout)
   http.HandleFunc("/contact", servecontact)
-  http.HandleFunc("/researchlinks", tester)
+  http.HandleFunc("/researchlinks", researchlinks)
   log.Fatal(s.ListenAndServe())
 }
 
-
-func tester(w http.ResponseWriter, r *http.Request){
-  tpl := template.Must(template.ParseFiles("researchlinks.gohtml","css/main.css","css/mcleod-reset.css"))
-  tpl.Execute(w, nil)
-}
-
 func serve(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("main.gohtml","css/main.css","css/mcleod-reset.css"))
+  tpl := template.Must(template.ParseFiles("main.gohtml","css/main.css","css/mcleod-reset.css"))
   tpl.Execute(w, nil)
 }
-
 func serveabout(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("about.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl := template.Must(template.ParseFiles("about.gohtml","css/main.css","css/mcleod-reset.css" ))
   tpl.Execute(w, nil)
 }
-
 func servecontact(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl := template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
+  tpl.Execute(w, nil)
+}
+func researchlinks(w http.ResponseWriter, r *http.Request){
+  tpl := template.Must(template.ParseFiles("researchlinks.gohtml","css/main.css","css/mcleod-reset.css"))
   tpl.Execute(w, nil)
 }
