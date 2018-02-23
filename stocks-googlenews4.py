4d4bf9379ee220edf648532500c248fc44027315
embulldogs99 <embulldogs99@yahoo.com>
2018-02-22 20:06:02 -0800

    automated posting

--- stocks-googlenews4.py
+++ stocks-googlenews4.py
@@ -816,7 +816,7 @@ def contentfilter():
 							conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 							cur = conn.cursor()
 							# execute a statement
-							cur.execute("INSERT INTO fmi.marketmentions (target, price, return, ticker, note, date, eps) VALUES (%d, %d, %%, %s, %s, %s, %s)", (targetprice,price,epsexpreturn,stock,grab,pub,str(value)))
+							cur.execute("INSERT INTO fmi.marketmentions (target, price, return, ticker, note, date, eps) VALUES (%s, %s, %%, %s, %s, %s, %s)", (targetprice,price,epsexpreturn,stock,grab,pub,str(value)))
 							print("inserted value")
 							conn.commit()
 							# close the communication with the PostgreSQL
@@ -834,7 +834,7 @@ def contentfilter():
 							conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 							cur = conn.cursor()
 							# execute a statement
-							cur.execute("INSERT INTO fmi.marketmentions (target, price, return, ticker, note, date) VALUES (%d, %d, %%, %s, %s, %s)", (value,price,predreturn*100,stock,grab,pub))
+							cur.execute("INSERT INTO fmi.marketmentions (target, price, return, ticker, note, date) VALUES (%s, %s, %%, %s, %s, %s)", (value,price,predreturn*100,stock,grab,pub))
 							print("inserted value")
 							conn.commit()
 							# close the communication with the PostgreSQL
