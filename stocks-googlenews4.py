acafe84967d138f76544113fb4522255e27595f1
embulldogs99 <embulldogs99@yahoo.com>
2018-02-22 19:01:04 -0800

    automated posting

--- stocks-googlenews4.py
+++ stocks-googlenews4.py
@@ -817,7 +817,7 @@ def contentfilter():
 
 
 							print("about to the databse")
-							conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
+							conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost host=/tmp/")
 							print("successfully connected")
 
 							cur = conn.cursor()
