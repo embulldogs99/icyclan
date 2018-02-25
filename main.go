32262760f6a0ccfd11091d95a045c4f055333b18
embulldogs99 <embulldogs99@yahoo.com>
2018-02-24 19:35:58 -0800

    automated posting

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func dbpull() []newspoint {
   }
 
 
-  rows, _ := db.Query("SELECT * FROM fmi.marketmentions WHERE Date > current_timestamp - interval '1 day' AND Report='analyst'")
+  rows, _ := db.Query("SELECT * FROM fmi.marketmentions WHERE Date > current_timestamp - interval '1 day'")
   bks := []newspoint{}
   for rows.Next() {
     bk := newspoint{}
