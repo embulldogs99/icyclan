22c8078a8e751e962408f63f6b5e10d094bb1149
embulldogs99 <embulldogs99@yahoo.com>
2018-03-22 22:18:52 -0700

    automated posting

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		email := r.FormValue("email")
 		pass := r.FormValue("pass")
-    fmt.Println(email.String + " logged on")
+    fmt.Println(email + " logged on")
 		//defines u as dbu user info (email,pass) then matches form email with stored email
 		u, ok := dbu[email]
 
