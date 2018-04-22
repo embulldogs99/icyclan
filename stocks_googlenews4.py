06da8f2f6e1b560b095a55803ae0c470db749925
embulldogs99 <embulldogs99@yahoo.com>
2018-04-21 23:46:23 -0700

    automated posting

--- stocks_googlenews4.py
+++ stocks_googlenews4.py
@@ -865,9 +865,7 @@ def contentfilter():
 
 
 #run for 100 cycles of 6 hours each
-for i in range(1,100):
-    mmduprem()
-    contentfilter()
-    mmduprem()
-    print('end')
-    time.sleep(86400/2)
+mmduprem()
+contentfilter()
+mmduprem()
+print('end')
