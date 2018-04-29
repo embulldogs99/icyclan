d44af5df9ab988418410f3f22d8d6a840a30032b
embulldogs99 <embulldogs99@yahoo.com>
2018-04-29 16:49:56 -0700

    automated posting

--- main.go
+++ main.go
@@ -323,7 +323,7 @@ type Portfolio struct{
 func portfoliopull() []Portfolio{
   db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
   if err != nil {log.Fatalf("Unable to connect to the database")}
-  sqlstatmt:="SELECT * FROM fmi.portfolio where ticker<>'CASH' ORDER BY exp_return dsc;"
+  sqlstatmt:="SELECT * FROM fmi.portfolio where ticker<>'CASH' ORDER BY exp_return desc;"
   rows, err := db.Query(sqlstatmt)
   if err != nil{log.Fatalf("failed to select portfolio")}
   bks := []Portfolio{}
