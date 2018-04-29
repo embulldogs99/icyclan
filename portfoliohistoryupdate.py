f82997b51998c52273ff22eec57a1000b50f8120
embulldogs99 <embulldogs99@yahoo.com>
2018-04-29 15:00:14 -0700

    automated posting

--- portfoliohistoryupdate.py
+++ portfoliohistoryupdate.py
@@ -37,7 +37,7 @@ def yahoopepuller(ticker):
 		return pe
 
 
-def quandl_snp500():
+def barchart_snp500():
     with requests.Session() as c:
         u='https://www.barchart.com/stocks/quotes/$SPX'
         x=c.get(u)
@@ -45,36 +45,36 @@ def quandl_snp500():
         titles=x.find_all()
         titles=str(titles)
         s=titles[titles.find("dailyLastPrice")+17:titles.find("dailyLastPrice")+17+10].replace('"','').replace(",","")
+        return float(s)
 
-        print(float(s))
-
-
-quandl_snp500()
-
-def quandl_nasdaq():
-    now=datetime.datetime.now()
-    currentdate=now.strftime("%Y-%m-%d")
-    data=quandl.get('NASDAQOMX/XQC', start_date=currentdate, end_date=currentdate)
-    print(data)
+def barchart_nasdaq():
+    with requests.Session() as c:
+        u='https://www.barchart.com/etfs-funds/quotes/QQQ'
+        x=c.get(u)
+        x=BeautifulSoup(x.content, "html.parser")
+        titles=x.find_all()
+        titles=str(titles)
+        s=titles[titles.find("dailyLastPrice")+17:titles.find("dailyLastPrice")+17+8].replace('"','').replace(",","")
+        return float(s)
 
 #############################################################################
 ############## Pull Current Portfolio and Obtain Tickers  ###################
-# conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
-# cur = conn.cursor()
-# cur.execute("""SELECT SUM(value) as total FROM fmi.portfolio;""")
-# portfoliovalues=cur.fetchall()
-# for row in portfoliovalues:
-#     portfoliovalue=row
-# snpvalue=quandl_snp500()
-# nasdaqvalue=quandl_nasdaq()
-# now=datetime.datetime.now()
-# currentdate=now.strftime("%Y-%m-%d")
-#
-# cur.execute("""INSERT INTO fmi.portfoliohistory (date,portfolio,snp,nasdaq) VALUES (%s,%s,%s,%s);""", (currentdate,portfoliovalue,snpvalue,nasdaqvalue))
-# conn.commit()
-#
-#
-#
-#
-# cur.close()
-# conn.close()
+conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
+cur = conn.cursor()
+cur.execute("""SELECT SUM(value) as total FROM fmi.portfolio;""")
+portfoliovalues=cur.fetchall()
+for row in portfoliovalues:
+    portfoliovalue=row
+snpvalue=barchart_snp500()
+nasdaqvalue=barchart_nasdaq()
+now=datetime.datetime.now()
+currentdate=now.strftime("%Y-%m-%d")
+
+cur.execute("""INSERT INTO fmi.portfoliohistory (date,portfolio,snp,nasdaq) VALUES (%s,%s,%s,%s);""", (currentdate,portfoliovalue,snpvalue,nasdaqvalue))
+conn.commit()
+
+
+
+
+cur.close()
+conn.close()
