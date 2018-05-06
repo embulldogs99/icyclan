7b33a4a27747c7937df78bc866b8a16b7ca3a1e6
embulldogs99 <embulldogs99@yahoo.com>
2018-05-06 00:50:17 -0700

    automated posting

--- portfoliovsbenchmark.py
+++ portfoliovsbenchmark.py
@@ -1,12 +1,10 @@
-# import Image
-# import matplotlib.pyplot as plt
 
-# plt.plot(range(10))
-# plt.savefig('testplot.png')
 # Image.open('testplot.png').save('testplot.jpg','JPEG')
 import psycopg2
 import pandas as pd
-
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 ############## Pull Current Portfolio and Obtain Tickers  ###################
 conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 cur = conn.cursor()
@@ -15,9 +13,22 @@ portfolio=cur.fetchall()
 
 data=pd.DataFrame(portfolio)
 
-
-print(data[[0]])
-
+portfolioreturns=data[[0]]
+snpreturns=data[[1]]
+nasdaqreturns=data[[2]]
 
 cur.close()
 conn.close()
+
+
+
+
+
+
+fig, ax = plt.subplots()
+ax.plot(portfolioreturns)
+ax.set(xlabel='Days', ylabel='Return',
+       title='Relative Portfolio Return')
+ax.grid()
+fig.savefig("test.png")
+plt.show()
