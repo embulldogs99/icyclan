5623c95cda87f6341d90d1cd87a20d1baf3fe960
embulldogs99 <embulldogs99@yahoo.com>
2018-05-06 00:43:27 -0700

    automated posting

--- portfoliovsbenchmark.py
+++ portfoliovsbenchmark.py
@@ -5,7 +5,7 @@
 # plt.savefig('testplot.png')
 # Image.open('testplot.png').save('testplot.jpg','JPEG')
 import psycopg2
-
+import pandas as pd
 
 ############## Pull Current Portfolio and Obtain Tickers  ###################
 conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
@@ -13,37 +13,10 @@ cur = conn.cursor()
 cur.execute("""SELECT portfolioreturn,snpreturn,nasdaqreturn FROM fmi.portfoliohistory;""")
 portfolio=cur.fetchall()
 
-start=1
-####################################################
-###Pull Quandl Price information for each ticker and send it to database#########
-###################################################
-port=["1","1"]
-snp=["1","1"]
-nasdaq=["1","1"]
-relport=1
-relsnp=1
-relnasdaq=1
-for pr,sr,nr in portfolio:
-    if start==1:
-        relport=start+(start*pr)
-        relsnp=start+(start*sr)
-        relnasdaq=start+(start*sr)
-        port=port.insert(0,relport)
-        snp=snp.insert(0,relsnp)
-        nasdaq=nasdaq.insert(0,relnasdaq)
-        start+=1
-    else:
-        relport=relport+(relport*pr)
-        relsnp=relsnp+(relsnp*sr)
-        relnasdaq=relnasdaq+(relnasdaq*sr)
-        port=port.insert(0,relport)
-        snp=snp.insert(0,relsnp)
-        nasdaq=nasdaq.insert(0,relnasdaq)
+data=pd.DataFrame(portfolio)
 
 
-print(port)
-print(snp)
-print(nasdaq)
+print(data)
 
 
 cur.close()
