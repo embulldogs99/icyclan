d1f73a4b956b6afe03405a663cb89526ad8de139
embulldogs99 <embulldogs99@yahoo.com>
2018-05-06 00:56:13 -0700

    automated posting

--- portfoliovsbenchmark.py
+++ portfoliovsbenchmark.py
@@ -20,11 +20,25 @@ nasdaqreturns=data[[2]]
 cur.close()
 conn.close()
 
+# Create some mock data
+t = np.arange(0.01, 10.0, 0.01)
+data1 = snpreturns
+data2 = portfolioreturns
 
-ax = plt.subplots()
-ax.plot(portfolioreturns)
-ax.set(xlabel='Days', ylabel='Return',
-       title='Relative Portfolio Return')
-ax.grid()
-fig.savefig("test.png")
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('exp', color=color)
+ax1.plot(t, data1, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+ax2.plot(t, data2, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
