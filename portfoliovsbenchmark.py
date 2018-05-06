bfe9d95d74b1e8f0b80fdf954899843d505dc2f8
embulldogs99 <embulldogs99@yahoo.com>
2018-05-06 01:03:06 -0700

    automated posting

--- portfoliovsbenchmark.py
+++ portfoliovsbenchmark.py
@@ -1,12 +1,12 @@
-
+import matplotlib
+matplotlib.use('Agg')
 # Image.open('testplot.png').save('testplot.jpg','JPEG')
 import psycopg2
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
+
 ############## Pull Current Portfolio and Obtain Tickers  ###################
 conn = psycopg2.connect("dbname='postgres' user='postgres' password='postgres' host='localhost' port='5432'")
 cur = conn.cursor()
