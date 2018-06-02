c85ed923ed9de0c905924f6923ef221931189905
embulldogs99 <embulldogs99@yahoo.com>
2018-06-01 20:16:36 -0700

    update

--- main.go
+++ main.go
@@ -316,7 +316,7 @@ func forums(w http.ResponseWriter, r *http.Request){
     fmt.Println(postcount)
     fmt.Println(current_time.Format("2006-01-02"))
 
-    _, err = db.Exec(`INSERT INTO icy.forums (postdate,postcount,poster,title,contents,imagefilelocation) VALUES ($1,$2,$3,$4,$5,$6);`,current_time.Format("2006-01-02").String(),postcount,u,posttitle,contents,imagefilelocation)
+    _, err = db.Exec(`INSERT INTO icy.forums (postdate,postcount,poster,title,contents,imagefilelocation) VALUES ($1,$2,$3,$4,$5,$6);`,current_time.Format("2006-01-02"),postcount,u,posttitle,contents,imagefilelocation)
     if err != nil{log.Fatalf("failed to insert new forums post")}
     db.Close()
     http.Redirect(w,r,"/forumscontent"+posttitle,http.StatusSeeOther)
