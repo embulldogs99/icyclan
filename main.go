c6135a4512da535d5e5ab496edd1575df7076784
embulldogs99 <embulldogs99@yahoo.com>
2018-06-02 00:48:07 -0700

    update

--- main.go
+++ main.go
@@ -331,7 +331,7 @@ func forums(w http.ResponseWriter, r *http.Request){
     db.Close()
     if err != nil{fmt.Println("failed to insert new forums post")}
 
-    http.Redirect(w,r,"/home",http.StatusSeeOther)
+    http.Redirect(w, r, "/login", http.StatusSeeOther)
 
   }
 
