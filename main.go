89e7073f871671ce96872f7d260e7a3a9e2fcd62
embulldogs99 <embulldogs99@yahoo.com>
2018-06-01 22:33:22 -0700

    update

--- main.go
+++ main.go
@@ -308,13 +308,11 @@ func forums(w http.ResponseWriter, r *http.Request){
 
 
     file, _, err:=r.FormFile("pic")
-    if err !=nil{http.Error(w,err.Error(),http.StatusInternalServerError)
-    return}
+    if err !=nil{http.Error(w,err.Error(),http.StatusInternalServerError)}
     defer file.Close()
     fmt.Printf("file uploaded")
-    f, er :=os.OpenFile("./forums/images/"+imagefilelocation, os.O_RDWR|os.O_CREATE, 0755)
-    if er != nil{fmt.Println(er)
-    return}
+    f, er :=os.Create("forums/images/"+imagefilelocation)
+    if er != nil{fmt.Println(er)}
     defer f.Close()
     io.Copy(f,file)
 
